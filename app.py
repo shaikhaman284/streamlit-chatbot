bd2af7730ee21a3129bcaf77dd9cf4c763aa3759
shaikhaman284 <75724876+shaikhaman284@users.noreply.github.com>
2025-10-22 12:09:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import time
 import hashlib
 
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.agents import AgentExecutor
-from langchain.agents import create_react_agent  # Try this first
+from langchain.agents import create_react_agent
+from langchain.agents import AgentExecutor  # Try this path
 from langchain_core.tools import Tool
 from langchain_core.prompts import PromptTemplate
 
